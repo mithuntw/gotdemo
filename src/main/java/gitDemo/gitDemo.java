f19565cc8ed9331295cbaaecc8586db9f90fdd03
mithunchannayanamath <mithun.c@thoughtworks.com>
2022-06-11 23:55:09 +0530

    it

--- src/main/java/gitDemo/gitDemo.java
+++ src/main/java/gitDemo/gitDemo.java
@@ -2,7 +2,7 @@ package gitDemo;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello hi");
+        System.out.println("Hello hi  jjjj");
         System.out.println("como");
     }
 }
