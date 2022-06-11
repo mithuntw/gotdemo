de58a0cc6c7db10382d409876aec12491fcb72cc
mithuntw <106316506+mithuntw@users.noreply.github.com>
2022-06-11 23:23:35 +0530

    Update gitDemo.java

--- src/main/java/gitDemo/gitDemo.java
+++ src/main/java/gitDemo/gitDemo.java
@@ -2,6 +2,6 @@ package gitDemo;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("Helllo micky hello hi");
+        System.out.println("Helllo micky hello hi ssup");
     }
 }
